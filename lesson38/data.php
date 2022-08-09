89639905b44e8e34a371c5071f49ce6ab81a6b0c
shipap <elyashiv.ner@gmail.com>
2022-08-09 11:37:01 +0300

    fix

--- lesson38/data.php
+++ lesson38/data.php
@@ -2,7 +2,7 @@
     $url = "https://rss.walla.co.il/feed/4703";
     $xml_string = file_get_contents($url);
 
-    $xml = simplexml_load_string($xml_string);
+    $xml = simplexml_load_string($xml_string, 'SimpleXMLElement', LIBXML_NOCDATA);
     $json = json_encode($xml);
     $array = json_decode($json, true);
 
