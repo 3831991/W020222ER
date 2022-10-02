09af4616ab1d70cbeab3ecabb71ed930d58c1415
shipap <elyashiv.ner@gmail.com>
2022-10-02 21:02:57 +0300

    lesson 49 + 50

--- lesson49/products.php
+++ lesson49/products.php
@@ -1,44 +1,72 @@
 <?
-    function getProducts() {
-        $host = "localhost";
-        $userName = "root";
-        $password = "";
-        $dbName = "full-stack";
+    $host = "localhost";
+    $userName = "root";
+    $password = "";
+    $dbName = "full-stack";
 
-        $link = mysqli_connect($host, $userName, $password, $dbName);
-        mysqli_set_charset($link, "utf8");
+    // יצירת החיבור למסד הנתונים
+    $link = mysqli_connect($host, $userName, $password, $dbName);
+    mysqli_set_charset($link, "utf8");
+
+    // פונקציה לקבלת המוצרים
+    function getProducts() {
+        global $link;
 
         $result = mysqli_query($link, "SELECT * FROM `products`");
 
         $arr = [];
 
         while ($row = mysqli_fetch_assoc($result)) {
+            $row['date'] = date('d/m/Y', strtotime($row['date']));
             $arr[] = $row;
         }
 
+        // מציגים את הפלט
         echo json_encode($arr);
     }
 
-    function deleteProduct() {
-        $host = "localhost";
-        $userName = "root";
-        $password = "";
-        $dbName = "full-stack";
+    // פונקציה למחיקת מוצר
+    function removeProduct() {
+        global $link;
+        
+        $id = mysqli_real_escape_string($link, $_POST['id']);
 
-        $link = mysqli_connect($host, $userName, $password, $dbName);
-        mysqli_set_charset($link, "utf8");
-
-        $result = mysqli_query($link, "DELETE FROM `products` WHERE `id`=" . $_POST['id']);
+        mysqli_query($link, "DELETE FROM `products` WHERE `id` = $id");
     }
+
+    function addProduct() {
+        global $link;
+
+        // פונקציה שדואגת שלא ייכנסו מזיקים למסד נתונים
+        // (מטפל בגרשיים שעלולים ליצור נזק לקוד)
+        $name = mysqli_real_escape_string($link, $_POST['name']);
+        $price = mysqli_real_escape_string($link, $_POST['price']);
+
+        mysqli_query($link, "INSERT INTO `products`(`name`, `price`, `date`) VALUES ('$name', '$price', CURRENT_DATE())");
+
+        // מקבל את המזהה של האלמנט האחרון שנוסף
+        $id = mysqli_insert_id($link);
+
+        // מקבלים מהמסד נתונים את המוצר האחרון שנוסף
+        $result = mysqli_query($link, "SELECT * FROM `products` WHERE `id` = $id");
+
+        // מציגים את הפלט
+        echo json_encode(mysqli_fetch_assoc($result));
+    }
+
 
     $action = '';
 
+    // אם יש פרמטר של פעולה, הוא שם אותו במשתנה
     if (isset($_POST['action'])) {
         $action = $_POST['action'];
     }
 
+    // מפעיל פונקציה לפי הפרמטר של הפעולה שקיבלנו מהג'אווה סקריפט
     if ($action == 'delete') {
-       deleteProduct();
+        removeProduct();
+    } else if ($action == 'add') {
+        addProduct();
     } else {
         getProducts();
     }
--- lesson49/script.js
+++ lesson49/script.js
@@ -1,27 +1,79 @@
 $(function() {
 
+    // מקבלים את כל המוצרים מהשרת
     $.get("products.php", function(res) {
+        // ממירים את הנתונים למערך
         const data = JSON.parse(res);
 
-        data.forEach((item, i) => {
-            $('#products').append(`
-                <tr id="${item.id}">
-                    <td>${i + 1}</td>
-                    <td>${item.name}</td>
-                    <td>${item.price}</td>
-                    <td>${item.date}</td>
-                    <td><button onclick="removeItem(this)" class="w3-btn w3-red">x</button></td>
-                </tr>
-            `);
+        // יוצרים מכל אובייקט שורה בטבלה
+        data.forEach(item => {
+            createHTML(item);
         });
     });
 
 });
 
+// פונקציה ליצירת שורה חדשה בטבלה
+function createHTML(item) {
+    $('#products').append(`
+        <tr id="${item.id}">
+            <td></td>
+            <td>${item.name}</td>
+            <td>${item.price}</td>
+            <td>${item.date}</td>
+            <td><button onclick="removeItem(this)" class="w3-btn w3-red remove">x</button></td>
+        </tr>
+    `);
+
+    // מפעילים את פונקצית מספור השורות
+    numbering();
+}
+
+// פונקציה הממספרת את הטבלה
+function numbering() {
+    $('#products tr').each(function(i) {
+        $(this).children().first().text(i + 1);
+    });
+}
+
 function removeItem(elem) {
+    // שואלים את הלקוח האם למחוק
+    if (!confirm("האם למחוק את המוצר?")) {
+        return;
+    }
+
+    // מזהה המוצר שאנחנו רוצים למחוק
     const id = $(elem).parents('tr').attr('id');
 
-    $.post("products.php", { id, action: 'delete' }, function(res) {
+    $.post("products.php", { id, action: 'delete' }, function() {
+        // מוחקים את השורה מהטבלה
+        $(elem).parents('tr').remove();
+        // מפעילים את פונקצית מספור השורות
+        numbering();
+    });
+}
 
+function addProduct() {
+    // קליטת הנתונים מהמודל
+    const name = $('#productName').val();
+    const price = $('#price').val();
+
+    // אם אין נתונים זה לא נותן להתקדם
+    if (!name || !price) {
+        alert("חסרים פרמטרים");
+        return;
+    }
+
+    // שליחת הנתונים לשרת
+    $.post("products.php", { name, price, action: 'add' }, function(res) {
+        const item = JSON.parse(res);
+
+        // מוסיף את המוצר לטבלה שלנו
+        createHTML(item);
+        // מסתיר את החלונית של ההוספה
+        $('#modal').hide();
+        // מוחקים את הערכים מהאינפוטים
+        $('#productName').val('');
+        $('#price').val('');
     });
 }
