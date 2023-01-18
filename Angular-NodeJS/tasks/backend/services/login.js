e8bb8a6beef48ff072438edb13ccba289a7a5d00
Your Name <you@example.com>
2023-01-18 20:55:44 +0200

    comments

--- Angular-NodeJS/tasks/backend/services/login.js
+++ Angular-NodeJS/tasks/backend/services/login.js
@@ -1,5 +1,6 @@
 import { con } from '../sqlConnect';
 
+// פונקציה לקבלת סטטוס החיבור של היוזר
 export function getLoginStatus(req, res) {
     if (req.session.user) {
         res.send({
@@ -14,18 +15,40 @@ export function getLoginStatus(req, res) {
 }
 
 export function logout(req, res) {
+    // בהתנתקות אנחנו פשוט מוחקים את המשתנה של היוזר מהסשיין
     delete req.session.user;
     res.send();
 }
 
 export function login(req, res) {
+    // בתור התחלה אנחנו מוחקים את המשתמש הנוכחי מהסשיין
     delete req.session.user;
+
+    // יותרים משתנה חדש בסשיין (אם לא קיים) - לצורך ספירת מספר ניסיונות החיבור
+    if (!req.session.attempts) {
+        req.session.attempts = 0;
+    }
+
+    // אם היוזר ניסה להתחבר יותר מ-7 פעמים - הוא נחסם
+    if (req.session.attempts >= 7) {
+        res.send({
+            status: 'error',
+            message: "נסיונות חיבור מרובים",
+        });
+
+        return;
+    }
+
     const sqlQuery = "SELECT * FROM `users` WHERE `userName`=? AND `password`=MD5(?)";
+    // trim: פונקציה המנקה רווחים מצידי הטקסט
     const paramArr = [req.body.userName.trim(), req.body.password.trim()];
 
     con.query(sqlQuery, paramArr, (err, result) => {
+        // אם יש שגיאה בשאליתה
         if (err) {
             console.log(err);
+
+            req.session.attempts++;
 
             res.send({
                 status: 'error',
@@ -35,12 +58,19 @@ export function login(req, res) {
             return;
         }
 
+        // אם אין אף יוזר העונה לשאילתא
         if (!result.length) {
+            req.session.attempts++;
+
             res.send({
                 status: 'error',
                 message: "שם משתמש או סיסמה לא נכונים",
             });
         } else {
+            // אם החיבור הצליח
+            // מנקים את המשנה שסופר נסיונות חיבור כושלים
+            delete req.session.attempts;
+
             const user = result[0];
             req.session.user = user;
 
