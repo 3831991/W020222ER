f578e5f0417f845549029c0da4d7da36a97b6c42
Your Name <you@example.com>
2023-02-05 18:40:11 +0200

    added

--- Angular-NodeJS/tasks/frontend/src/app/navbar/navbar.component.ts
+++ Angular-NodeJS/tasks/frontend/src/app/navbar/navbar.component.ts
@@ -5,15 +5,25 @@ import { Nav } from './navbar.interface';
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.css']
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
     active: string = '';
+    isOpen: boolean = false;
 
     menu: Nav[] = [
         { route: '/', title: 'בית', icon: 'home' },
         { route: '/tasks', title: 'משימות', icon: 'check-square-o' },
     ];
+
+    sidebar: Nav[] = [
+        ...this.menu,
+        { route: '/', title: 'ירקות', icon: 'home' },
+        { route: '/', title: 'פירות', icon: 'home' },
+        { route: '/', title: 'קטניות', icon: 'home' },
+        { route: '/', title: 'ירוקים', icon: 'home' },
+        { route: '/', title: 'פירות יבשים', icon: 'home' },
+    ]
 
     constructor(router: Router) {
 
